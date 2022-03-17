c171d225552e2138d003269f110ac02426de938c
Muhammad Rehman <rehmandomore@gmail.com>
2022-03-17 19:18:38 +0500

    initial commit

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const UserProfile = ({ name }: Props) => {
   return (
-    <div className="flex items-center space-x-6">
+    <div className="flex items-center space-x-7">
       <p className="text-[14px] font-medium ml-5">Shekh AI Raihan</p>
       <span className="outline-2 outline rounded-full p-[2px] outline-secondary">
         <img src="/user.png" alt="user-img" className="w-10 h-10" />
